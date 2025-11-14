7bb19ad50a408936d26bf1ee6bca20869505ac48
nikborovikov <nikborovikov@team.amocrm.com>
2025-11-14 16:16:12 +0300

    refactor dto

--- internal/infrastructure/transport/http/dto/request.go
+++ internal/infrastructure/transport/http/dto/request.go
@@ -2,29 +2,10 @@ package dto
 
 import "avitoTestTask/internal/core/models"
 
+// Team Request DTO
 type CreateTeamRequest struct {
 	TeamName string   `json:"team_name"`
 	Members  []Member `json:"members"`
-}
-
-type SetUserActiveRequest struct {
-	UserID   string `json:"user_id"`
-	IsActive bool   `json:"is_active"`
-}
-
-type CreatePRRequest struct {
-	PullRequestID   string `json:"pull_request_id"`
-	PullRequestName string `json:"pull_request_name"`
-	AuthorID        string `json:"author_id"`
-}
-
-type MergePRRequest struct {
-	PullRequestID string `json:"pull_request_id"`
-}
-
-type ReassignPRRequest struct {
-	PullRequestID string `json:"pull_request_id"`
-	OldUserID     string `json:"old_reviewer_id"`
 }
 
 type Member struct {
@@ -32,8 +13,6 @@ type Member struct {
 	Username string `json:"username"`
 	IsActive bool   `json:"is_active"`
 }
-
-// TODO: нужно сделать валидацию required полей
 
 func (r *CreateTeamRequest) ToDomainTeam() models.Team {
 	users := make([]models.User, 0, len(r.Members))
@@ -53,11 +32,33 @@ func (r *CreateTeamRequest) ToDomainTeam() models.Team {
 	}
 }
 
+// PR Request DTO
+type CreatePRRequest struct {
+	PullRequestID   string `json:"pull_request_id"`
+	PullRequestName string `json:"pull_request_name"`
+	AuthorID        string `json:"author_id"`
+}
+
+type MergePRRequest struct {
+	PullRequestID string `json:"pull_request_id"`
+}
+
+type ReassignPRRequest struct {
+	PullRequestID string `json:"pull_request_id"`
+	OldUserID     string `json:"old_reviewer_id"`
+}
+
 func (r *CreatePRRequest) ToDomainPR() models.PullRequest {
 	return models.PullRequest{
 		ID:       r.PullRequestID,
 		Title:    r.PullRequestName,
 		AuthorID: r.AuthorID,
-		Status:   models.PRStatusOpen, //по умолчанию
+		Status:   models.PRStatusOpen, //по умолчанию статус Open
 	}
+}
+
+// User Request DTO
+type SetUserActiveRequest struct {
+	UserID   string `json:"user_id"`
+	IsActive bool   `json:"is_active"`
 }
--- internal/infrastructure/transport/http/dto/response.go
+++ internal/infrastructure/transport/http/dto/response.go
@@ -5,6 +5,20 @@ import (
 	"time"
 )
 
+// Main Response DTO
+type ErrorResponse struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
+func NewErrorResponse(code, msg string) ErrorResponse {
+	return ErrorResponse{
+		Code:    code,
+		Message: msg,
+	}
+}
+
+// Team Response DTO
 type CreateTeamOKResponse struct {
 	Team Team `json:"team"`
 }
@@ -14,32 +28,53 @@ type GetTeamByNameResponse struct {
 	Members  []Member `json:"members"`
 }
 
-type GetUserReviewPRResponse struct {
-	UserID       string             `json:"user_id"`
-	PullRequests []PullRequestShort `json:"pull_requests"`
-}
-
-type SetUserActiveResponse struct {
-	User User `json:"user"`
-}
-
-type User struct {
-	UserID   string `json:"user_id"`
-	Username string `json:"username"`
-	TeamName string `json:"team_name"`
-	IsActive bool   `json:"is_active"`
-}
-
-type ErrorResponse struct {
-	Code    string `json:"code"`
-	Message string `json:"message"`
-}
-
 type Team struct {
 	TeamName string   `json:"team_name"`
 	Members  []Member `json:"members"`
 }
 
+func NewCreateTeamOKResponse(team *models.Team) *CreateTeamOKResponse {
+	if team == nil {
+		return nil
+	}
+	members := convertUsersToMembers(team.Users)
+	return &CreateTeamOKResponse{
+		Team: Team{
+			TeamName: team.Name,
+			Members:  members,
+		},
+	}
+}
+
+func NewGetTeamByNameResponse(team *models.Team) *GetTeamByNameResponse {
+	if team == nil {
+		return nil
+	}
+	members := convertUsersToMembers(team.Users)
+	return &GetTeamByNameResponse{
+		TeamName: team.Name,
+		Members:  members,
+	}
+}
+
+func convertUsersToMembers(users []models.User) []Member {
+	if len(users) == 0 {
+		return []Member{}
+	}
+
+	members := make([]Member, 0, len(users))
+	for _, user := range users {
+		member := Member{
+			UserID:   user.ID,
+			Username: user.Name,
+			IsActive: user.IsActive,
+		}
+		members = append(members, member)
+	}
+	return members
+}
+
+// PR Response DTO
 type PullRequestShort struct {
 	PullRequestID   string          `json:"pull_request_id"`
 	PullRequestName string          `json:"pull_request_name"`
@@ -55,76 +90,80 @@ type ReassignPRResponse struct {
 	PullRequest PullRequest `json:"pr"`
 }
 
+type MergePRResponse struct {
+	PullRequest PullRequest `json:"pr"`
+}
+
 type PullRequest struct {
 	PullRequestID     string          `json:"pull_request_id"`
 	PullRequestName   string          `json:"pull_request_name"`
 	AuthorID          string          `json:"author_id"`
 	Status            models.PRStatus `json:"status"`
 	AssignedReviewers []string        `json:"assigned_reviewers"`
+	MergedAt          time.Time       `json:"mergedAt,omitempty"`
 }
 
-type MergePRResponse struct {
-	PullRequest PR `json:"pr"`
-}
-
-type PR struct {
-	PullRequestID     string          `json:"pull_request_id"`
-	PullRequestName   string          `json:"pull_request_name"`
-	AuthorID          string          `json:"author_id"`
-	Status            models.PRStatus `json:"status"`
-	AssignedReviewers []string        `json:"assigned_reviewers"`
-	MergedAt          time.Time       `json:"mergedAt"`
-}
-
-func NewErrorResponse(code, msg string) ErrorResponse {
-	return ErrorResponse{
-		Code:    code,
-		Message: msg,
-	}
-}
-
-func NewCreateTeamOKResponse(team *models.Team) *CreateTeamOKResponse {
-	if team == nil {
+func NewCreatePRResponse(pr *models.PullRequest) *CreatePRResponse {
+	if pr == nil {
 		return nil
 	}
-
-	members := make([]Member, 0, len(team.Users))
-
-	for _, user := range team.Users {
-		member := Member{
-			UserID:   user.ID,
-			Username: user.Name,
-			IsActive: user.IsActive,
-		}
-		members = append(members, member)
-	}
-	return &CreateTeamOKResponse{
-		Team: Team{
-			TeamName: team.Name,
-			Members:  members,
+	return &CreatePRResponse{
+		PullRequest: PullRequest{
+			PullRequestID:     pr.ID,
+			PullRequestName:   pr.Title,
+			AuthorID:          pr.AuthorID,
+			Status:            pr.Status,
+			AssignedReviewers: pr.Reviewers,
 		},
 	}
 }
 
-func NewGetTeamByNameResponse(team *models.Team) *GetTeamByNameResponse {
-	if team == nil {
+func NewMergePRResponse(pr *models.PullRequest) *MergePRResponse {
+	if pr == nil {
 		return nil
 	}
-
-	members := make([]Member, 0, len(team.Users))
-
-	for _, user := range team.Users {
-		member := Member{
-			UserID:   user.ID,
-			Username: user.Name,
-			IsActive: user.IsActive,
-		}
-		members = append(members, member)
+	return &MergePRResponse{
+		PullRequest: PullRequest{
+			PullRequestID:     pr.ID,
+			PullRequestName:   pr.Title,
+			AuthorID:          pr.AuthorID,
+			Status:            pr.Status,
+			AssignedReviewers: pr.Reviewers,
+			MergedAt:          pr.MergedAt,
+		},
 	}
-	return &GetTeamByNameResponse{
-		TeamName: team.Name,
-		Members:  members,
+}
+
+func NewReassignPRResponse(pr *models.PullRequest) *ReassignPRResponse {
+	if pr == nil {
+		return nil
 	}
+	return &ReassignPRResponse{
+		PullRequest: PullRequest{
+			PullRequestID:     pr.ID,
+			PullRequestName:   pr.Title,
+			AuthorID:          pr.AuthorID,
+			Status:            pr.Status,
+			AssignedReviewers: pr.Reviewers,
+		},
+	}
+}
+
+// User Response DTO
+type GetUserReviewPRResponse struct {
+	UserID       string             `json:"user_id"`
+	PullRequests []PullRequestShort `json:"pull_requests"`
+}
+
+type SetUserActiveResponse struct {
+	User User `json:"user"`
+}
+
+type User struct {
+	UserID   string `json:"user_id"`
+	Username string `json:"username"`
+	TeamName string `json:"team_name"`
+	IsActive bool   `json:"is_active"`
 }
 
 func NewGetUserReviewPRResponse(userID string, prs []models.PullRequest) *GetUserReviewPRResponse {
@@ -149,62 +188,12 @@ func NewSetUserActiveResponse(teamName string, user *models.User) *SetUserActive
 	if user == nil {
 		return nil
 	}
-
 	return &SetUserActiveResponse{
 		User: User{
 			UserID:   user.ID,
 			Username: user.Name,
 			TeamName: teamName,
 			IsActive: user.IsActive,
-		},
-	}
-}
-
-func NewCreatePRResponse(pr *models.PullRequest) *CreatePRResponse {
-	if pr == nil {
-		return nil
-	}
-
-	return &CreatePRResponse{
-		PullRequest: PullRequest{
-			PullRequestID:     pr.ID,
-			PullRequestName:   pr.Title,
-			AuthorID:          pr.AuthorID,
-			Status:            pr.Status,
-			AssignedReviewers: pr.Reviewers,
-		},
-	}
-}
-
-func NewMergePRResponse(pr *models.PullRequest) *MergePRResponse {
-	if pr == nil {
-		return nil
-	}
-
-	return &MergePRResponse{
-		PullRequest: PR{
-			PullRequestID:     pr.ID,
-			PullRequestName:   pr.Title,
-			AuthorID:          pr.AuthorID,
-			Status:            pr.Status,
-			AssignedReviewers: pr.Reviewers,
-			MergedAt:          pr.MergedAt,
-		},
-	}
-}
-
-func NewReassignPRResponse(pr *models.PullRequest) *ReassignPRResponse {
-	if pr == nil {
-		return nil
-	}
-
-	return &ReassignPRResponse{
-		PullRequest: PullRequest{
-			PullRequestID:     pr.ID,
-			PullRequestName:   pr.Title,
-			AuthorID:          pr.AuthorID,
-			Status:            pr.Status,
-			AssignedReviewers: pr.Reviewers,
 		},
 	}
 }
